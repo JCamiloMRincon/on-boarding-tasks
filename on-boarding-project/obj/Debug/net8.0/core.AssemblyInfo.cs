54f00cf001f5d389504cb8a43b71e317c3f9ab6d
CamiloMortigo <camilomortigo@gmail.com>
2025-03-21 09:20:48 -0500

    Changes

--- on-boarding-project/obj/Debug/net8.0/core.AssemblyInfo.cs
+++ on-boarding-project/obj/Debug/net8.0/core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49dd0cee894540f1e2121a88abae365cbcc02bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+751809cbb62191f9f6156a92fee3512c529ab930")]
 [assembly: System.Reflection.AssemblyProductAttribute("core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

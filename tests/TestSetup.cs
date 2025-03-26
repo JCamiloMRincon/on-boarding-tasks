ec70de1b652fdb7929c95cba0f0cfd1284ae6527
JCamiloMRincon <camilomortigo@gmail.com>
2025-03-26 16:54:20 -0500

    Add Setup Fixture class

---
+++ tests/TestSetup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using System.IO;
+using Allure.Commons;
+
+[SetUpFixture]
+public class TestSetup
+{
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        string resultsDirectory = Path.Combine(TestContext.CurrentContext.WorkDirectory, "allure-results");
+        Environment.SetEnvironmentVariable("ALLURE_RESULTS_DIRECTORY", resultsDirectory);
+    }
+}
+
